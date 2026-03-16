78a8f935cb4aebaceb87506196f79acf147c6ebb
ignfernandez_meli <ignacioagustin.fernandez@mercadolibre.com>
2026-03-15 22:35:42 -0300

    fix nil address access

--- client/common/client.go
+++ client/common/client.go
@@ -48,6 +48,7 @@ func (c *Client) createClientSocket() error {
 			c.config.ID,
 			err,
 		)
+		return err
 	}
 	c.conn = conn
 	return nil
@@ -64,7 +65,9 @@ func (c *Client) StartClientLoop() {
 
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
 		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
+		if err := c.createClientSocket(); err != nil {
+			return
+		}
 
 		// TODO: Modify the send to avoid short-write
 		fmt.Fprintf(
